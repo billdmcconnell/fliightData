634e9f3398a37fde012f84056467c0573339b877
jgoley <jgoley@gmail.com>
2016-12-28 16:45:44 -0500

    Update styles of layer selectors

--- app/packages/grits-net-meteor/package.js
+++ app/packages/grits-net-meteor/package.js
@@ -51,6 +51,7 @@ Package.on_use(function(api){
     'client/stylesheets/variables.import.styl',
     'client/stylesheets/mixins.import.styl',
     'client/stylesheets/globals.import.styl',
+    'client/stylesheets/layers.import.styl',
     'client/stylesheets/sidebar.import.styl',
     'client/stylesheets/sidebar_table.import.styl',
     'client/stylesheets/main.styl',
